8e1afb5cb600a047d13d426f3d925c82f3391153
j-toscani <julian_toscani@gmx.de>
2019-12-04 12:44:23 +0100

    add GlobalStyle-file

---
+++ client/src/themes/darkTheme.js
@@ -0,0 +1,9 @@
+const darkTheme = {
+  main: "#9B7D53",
+  contrast: "#514637",
+  accent: "rgba(0,0,0,0.9)",
+  mainFont: "#DFDFDF",
+  accentFont: "rgba(0,0,0,0.9)"
+};
+
+export default darkTheme;
