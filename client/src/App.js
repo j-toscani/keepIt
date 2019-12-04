e9cfeb505b14546716364fcef6718fc976304b83
Julian Toscani <julian_toscani@gmx.de>
2019-12-04 13:19:54 +0100

    erase boilerplate files & add colors + logo (#14)
    * erase boilerplate files
    
    * add and implement favicon
    
    * erase boilerplate code
    
    *  add styled components
    
    * add roboto font
    
    *  add first draft of themes and GlobalStyle-files
    
    * add samplecode in appJS to check functionality,
    
    closes #6

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import GlobalStyles from "./GlobalStyles";
+import colorTheme from "./themes/colorTheme";
+import darkTheme from "./themes/darkTheme";
+
+const Container = styled.div`
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template: 80px 1fr / 1fr;
+`;
 
 function App() {
+  const [darkmode, setDarkmode] = React.useState(false);
+
+  function toggleTheme() {
+    setDarkmode(!darkmode);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={darkmode ? darkTheme : colorTheme}>
+      <Container>
+        <GlobalStyles />
+        <header style={{ border: "1pt black solid" }} black>
+          I am a header
+        </header>
+        <main>
+          <h1>I am a h1 in a Main</h1>
+          <button onClick={toggleTheme}> Click to change colors</button>
+        </main>
+      </Container>
+    </ThemeProvider>
   );
 }
 
---
+++ client/src/themes/colorTheme.js
@@ -0,0 +1,9 @@
+const colorTheme = {
+  main: "#F2D4AE",
+  contrast: "#BF9E75",
+  accent: "#402B18",
+  mainFont: "#FFFFFF",
+  accentFont: "#000000"
+};
+
+export default colorTheme;
