b3d8cd4b747f4ae74a0c08301d3799226a39e3c7
j-toscani <julian_toscani@gmx.de>
2019-12-04 15:23:00 +0100

    add header and basic header-components, fixes #17

---
+++ client/src/components/BurgerButton.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+import Options from "../ressources/Options";
+
+const StyledButton = styled.button`
+  height: 40px;
+  width: 40px;
+  padding: 5px;
+  margin: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background: none;
+  border: none;
+`;
+
+export default function BurgerButton({ handleClick }) {
+  return (
+    <StyledButton onClick={handleClick}>
+      <Options />
+    </StyledButton>
+  );
+}
---
+++ client/src/stories/1-Header.stories.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default {
+  title: "Header"
+};
+
+export const Default = () => <Top />;
