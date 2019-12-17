836955bb86b79226d0b50b399b7836ba683e0092
Julian Toscani <julian_toscani@gmx.de>
2019-12-17 09:30:26 +0100

    add p-element and some styling (#23)
    closes #15

---
+++ client/src/components/OutputTest.js
@@ -0,0 +1,44 @@
+import React from "react";
+import styled from "styled-components";
+
+const StyledForm = styled.form`
+  background: ${props => props.theme.accent};
+  max-width: 280px;
+  margin-top: auto;
+  padding: 20px;
+  display: flex;
+  justify-content: space-around;
+  min-height: 200px;
+  align-items: center;
+  flex-direction: column;
+  border-radius: 10px;
+  color: ${props => props.theme.mainFont};
+`;
+
+const Container = styled.section`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+`;
+
+const TextField = styled.p`
+  margin: 20px;
+  border: 1px solid black;
+  padding: 10px;
+`;
+
+export default function OutputTest() {
+  return (
+    <Container>
+      <StyledForm>
+        <label htmlFor="noteId">ID</label>
+        <input name="noteId" placeholder="Note-ID"></input>
+        <button type="submit">Get note...</button>
+      </StyledForm>
+      <TextField>
+        99999999999999999999999999999999999999999999999999999999999999999999999999999999
+      </TextField>
+    </Container>
+  );
+}
