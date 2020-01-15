22f2e575a6010ee377bbfb6d07d5e7ec1d2e152f
Leon Machens <leon.machens@googlemail.com>
2020-01-15 09:02:16 +0100

    Remove comment

--- client/src/components/StyledHeader.js
+++ client/src/components/StyledHeader.js
@@ -1,8 +1,4 @@
-// For some reason, parcel won't allow process.env.NODE_ENV access (process is undefined) if there is not "process" in the file.
-// @emotion/babel-preset-css-prop will transform this file and access process.env.NODE_ENV (see https://emotion.sh/docs/@emotion/babel-preset-css-prop#example), but parcel will not notice (https://en.parceljs.org/env.html#%F0%9F%8C%B3-environment-variables).
-// A workaround is to write "process" in this file (comment works):
 // process;
-// I would recommend to create a Parcel issue.
 
 import React from "react";
 import { css } from "@emotion/core";
