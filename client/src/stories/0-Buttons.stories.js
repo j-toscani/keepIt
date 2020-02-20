8024838155b304fd348a296bf6f2f0ac60905367
j-toscani <julian_toscani@gmx.de>
2020-02-20 22:01:25 +0100

    redo button story

--- client/src/stories/0-Buttons.stories.js
+++ client/src/stories/0-Buttons.stories.js
@@ -1,10 +1,23 @@
 import React from "react";
 import Button from "../components/Button";
+import Option from "../ressources/Options";
 
 export default {
   title: "Buttons"
 };
 
 export const MenuButton = () => (
-  <Button handleClick={() => alert("Clicked!")} />
+  <Button
+    burgerButton
+    children={<Option />}
+    handleClick={() => alert("Clicked!")}
+  />
+);
+
+export const DefaultButton = () => (
+  <Button
+    burgerButton
+    children={"Click me!"}
+    handleClick={() => alert("Clicked!")}
+  />
 );
