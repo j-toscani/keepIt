f86ec1260c6734898867b6c5bb76ceee82e21f40
j-toscani <julian_toscani@gmx.de>
2020-02-21 18:11:19 +0100

    create input label with first capital letter

---
+++ client/src/components/loginAndRegistryComponents/CategoryBumper.js
@@ -0,0 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+export default function CategoryBumper({ children }) {
+  return <div>{children}</div>;
+}
---
+++ client/src/components/loginAndRegistryComponents/LoginOrRegistryContainer.js
@@ -0,0 +1,6 @@
+/** @jsx jsx */
+import { css, jsx } from "@emotion/core";
+
+export default function LoginOrRegistryContainer({ children }) {
+  return <div>{children}</div>;
+}
