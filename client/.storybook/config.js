31bdaa6bdf421450723b5615fb5f5a79083cf584
j-toscani <julian_toscani@gmx.de>
2020-02-26 19:36:49 +0100

    fixed storybook

--- client/.storybook/config.js
+++ client/.storybook/config.js
@@ -1,24 +1,20 @@
-import React from "react";
+import React, { Fragment } from "react";
 import { configure, addDecorator } from "@storybook/react";
 import ThemeProvider from "../src/themes/ThemeContext";
 import GlobalStyles from "../src/GlobalStyles";
 
 // automatically import all files ending in *.stories.js
-configure(require.context("../src/stories", true, /\.stories\.js$/), module);
-
-const Main = styled.div`
-  margin: 10px;
-`;
+configure(require.context("../stories", true, /\.stories\.js$/), module);
 
 // add GlobalStyle for every story
 const GlobalStyleDecorator = storyFn => (
   <Fragment>
     <ThemeProvider>
-      <Main>
+      <main>
         <GlobalStyles />
 
         {storyFn()}
-      </Main>
+      </main>
     </ThemeProvider>
   </Fragment>
 );
--- client/stories/1-FormElements.stories.js
+++ client/stories/1-FormElements.stories.js
@@ -7,16 +7,18 @@ export default {
 };
 
 export const TextInput = () => (
-  <SmallTextInput
-    inputAttribute={"name"}
-    onClick={() =>
-      alert(
-        `You clicked the Textinput with the following props: InputAttribute:${inputAttribute}, placeholder:${inputAttribute}, inputLabel:${
-          inputLabel ? inputLabel : "Fallback value of inputLabel"
-        }`
-      )
-    }
-  />
+  <div style={{ width: "100px" }}>
+    <SmallTextInput
+      inputAttribute={"name"}
+      onClick={() =>
+        alert(
+          `You clicked the Textinput with the following props: InputAttribute:${inputAttribute}, placeholder:${inputAttribute}, inputLabel:${
+            inputLabel ? inputLabel : "Fallback value of inputLabel"
+          }`
+        )
+      }
+    />
+  </div>
 );
 
 export const Label = () => (
