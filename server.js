b5cc7cf387614e94519134b88ec27e0c4aedee8e
j-toscani <julian_toscani@gmx.de>
2020-03-10 19:22:55 +0100

    allow all origins

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 const dbURL = process.env.DB_URL || "mongodb://localhost:27017";
 const dbName = process.env.DB_NAME || "MyNotes";
 
-app.use(cors({ origin: `${process.env.HOST_URL}` || "*" }));
+app.use(cors());
 
 app.use(express.json());
 
